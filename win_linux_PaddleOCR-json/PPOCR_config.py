99bc97300132ff0b2c461d07cc9358ecbf53d3da
unknown <2230247019@qq.com>
2024-07-10 11:12:39 +0800

    修改PaddleOCR-json插件默认设置参数值： - CPU线程数：默认最多16线程 - 内存占用：默认最多8G - 内存选项默认不隐藏

--- win_linux_PaddleOCR-json/PPOCR_config.py
+++ win_linux_PaddleOCR-json/PPOCR_config.py
@@ -25,7 +25,9 @@ def _getlanguageList():
                 optionsList.append([f"models/{parts[0]}", parts[1]])
         return optionsList
     except FileNotFoundError:
-        print("[Error] PPOCR配置文件configs不存在，请检查文件路径是否正确。", configsPath)
+        print(
+            "[Error] PPOCR配置文件configs不存在，请检查文件路径是否正确。", configsPath
+        )
     except IOError:
         print("[Error] PPOCR配置文件configs无法打开或读取。")
     return []
@@ -34,8 +36,9 @@ def _getlanguageList():
 _LanguageList = _getlanguageList()
 
 
-# 获取最佳线程数
+# 获取最佳线程数。用户设定可以覆盖这个计算值。
 def _getThreads():
+    threadsCount = 1
     try:
         phyCore = psutil.cpu_count(logical=False)  # 物理核心数
         lgiCore = psutil.cpu_count(logical=True)  # 逻辑核心数
@@ -47,16 +50,43 @@ def _getThreads():
             raise ValueError("核心数计算异常")
         # 物理核数=逻辑核数，返回逻辑核数
         if phyCore * 2 == lgiCore or phyCore == lgiCore:
-            return lgiCore
+            threadsCount = lgiCore
         # 大小核处理器，返回大核线程数
-        big = lgiCore - phyCore
-        return big * 2
+        else:
+            big = lgiCore - phyCore
+            threadsCount = big * 2
+        threadsCount = int(threadsCount)
     except Exception as e:
         print("[Warning] 无法获取CPU核心数！", e)
-        return 4
+    # 线程上限16
+    if threadsCount > 16:
+        threadsCount = 16
+    return threadsCount
 
 
 _threads = _getThreads()
+
+
+# 获取内存占用默认上限。用户设定可以覆盖这个计算值。
+def _getRamMax():
+    ramMax = 1024
+    try:
+        # 获取系统总内存数（以字节为单位）
+        totalMemoryBytes = psutil.virtual_memory().total
+        # 将总内存数转换为 MB 单位
+        ramMax = totalMemoryBytes / 1048576
+        ramMax = int(ramMax)
+    except Exception as e:
+        print("[Warning] 无法获取系统总内存数！", e)
+    # 内存下限200MB，上限8G
+    if ramMax < 200:
+        ramMax = 200
+    elif ramMax > 8129:
+        ramMax = 8129
+    return ramMax
+
+
+_ramMax = _getRamMax()
 
 globalOptions = {
     "title": tr("PaddleOCR（本地）"),
@@ -64,7 +94,9 @@ globalOptions = {
     "enable_mkldnn": {
         "title": tr("启用MKL-DNN加速"),
         "default": True,
-        "toolTip": tr("使用MKL-DNN数学库提高神经网络的计算速度。能大幅加快OCR识别速度，但也会增加内存占用。"),
+        "toolTip": tr(
+            "使用MKL-DNN数学库提高神经网络的计算速度。能大幅加快OCR识别速度，但也会增加内存占用。"
+        ),
     },
     "cpu_threads": {
         "title": tr("线程数"),
@@ -74,20 +106,18 @@ globalOptions = {
     },
     "ram_max": {
         "title": tr("内存占用限制"),
-        "default": -1,
+        "default": _ramMax,
         "min": -1,
         "unit": "MB",
         "isInt": True,
-        "advanced": True,
         "toolTip": tr("值>0时启用。引擎内存占用超过该值时，执行内存清理。"),
     },
     "ram_time": {
         "title": tr("内存闲时清理"),
-        "default": 30,
+        "default": 60,
         "min": -1,
         "unit": tr("秒"),
         "isInt": True,
-        "advanced": True,
         "toolTip": tr("值>0时启用。引擎空闲时间超过该值时，执行内存清理。"),
     },
 }
@@ -112,6 +142,8 @@ localOptions = {
             [4320, "4320"],
             [999999, tr("无限制")],
         ],
-        "toolTip": tr("将边长大于该值的图片进行压缩，可以提高识别速度。可能降低识别精度。"),
+        "toolTip": tr(
+            "将边长大于该值的图片进行压缩，可以提高识别速度。可能降低识别精度。"
+        ),
     },
 }
